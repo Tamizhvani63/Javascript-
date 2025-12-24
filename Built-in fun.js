6e5710e098c8fec74d09338981b9999df9f0d5b5
tamizhvani <tamizhvani63@gmail.com>
2025-12-24 07:53:43 +0700

    first commit

---
+++ Built-in fun.js
@@ -0,0 +1,22 @@
+// Math Function (In-Built Function)
+
+let c;
+c=Math.PI;
+c=Math.E;
+c=Math.round(5.8); //6
+c=Math.floor(5.3); //its  remove the valve after decimal
+c=Math.ceil(5.3); // print next greatest value o/p 6
+c=Math.sqrt(90);
+c = Math.abs(-85);
+c=Math.trunc(2.58); // its print integer num
+c=Math.pow(2,4) //16
+c=Math.min(10,20,3,40,50) //3
+c = Math.max(10, 20, 3, 40, 50);//50
+c=Math.random(); //print random number
+c=Math.floor(Math.random()*50+1);
+c=Math.sign(25); //if its positive ans 1, or  it show negative -1 ,its 0 means 0.
+c= Math.sin(55);
+c = Math.cos(45);
+c = Math.log(50);
+c = Math.log2(35);
+console.log(c) // o/p --->2.7182818284...
